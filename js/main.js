bb043ec12a9e5c7a7abd93be4118c5f6e4a24fb5
Ramunas Alksnys <ramunas.alksnys@gmail.com>
2018-02-18 17:53:24 +0200

    Some animation to travel's badgets

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-function enlarge(picId, picP) {
-    $('#'+picId).css({ 'height': '350px' }); $('#'+picP).css({ 'width': 'calc(100% - 60px)', "background-color":"#443857", "color":"white", "padding": "5px 0", "font-size":"20px" });   
 
-}
-function shrink(picId, picP) {
-    $('#'+picId).css({ 'height': '300px' }); $('#'+picP).css({ 'width': 'calc(100% - 80px)', "background-color":"white", "color":"#337ab7", "padding": "5px 0", "font-size":"14px"   });
-}
+function badget() {
+    $(".badget").hover(function () {
+        $(this).find("img").fadeIn().css({ 'height': '350px' });
+        $(this).find("p").fadeIn().css({ 'width': 'calc(100% - 60px)', "background-color": "#443857", "color": "white", "padding": "5px 0", "font-size": "20px" });
+    },
+        function () {
+            $(this).find("img").css({ 'height': '300px' });
+            $(this).find("p").css({ 'width': 'calc(100% - 80px)', "background-color": "white", "color": "#337ab7", "padding": "5px 0", "font-size": "14px" });
+        }).click(function(){
+            $(this).parent().find(".content").slideToggle("slow");
+        });
+
+};
